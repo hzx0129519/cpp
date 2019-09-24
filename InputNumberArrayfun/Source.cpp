879721d1e62ccc414c58cb1a47af7c0b391467c6
hzx <zixuan@u.nus.edu>
2019-09-24 16:03:59 +0800

    updated

---
+++ InputNumberArrayfun/Source.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<iomanip> // for formatting the output
+#include<cmath> // for calculation 
+
+using namespace std;
+
+struct myArray {
+	const size_t initial_size = 1;
+	const size_t growth_factor = 2;
+	size_t length = 0;
+	size_t capacity = initial_size;
+
+	int* v; // vector
+};
+
+int main() {
+	int x; 
+	myArray numbers;
+	
+	while (true) {
+		cout << "Please input a number (-ve number terminates the program)" << endl;
+		if (!(cin >> x) || (x < 0)) break;
+
+		if (numbers.length == numbers.capacity) {
+			int* temp = new int[numbers.capacity *= numbers.growth_factor];
+			for (size_t i = 0; i < numbers.length; ++i)
+				temp[i] = numbers.v[i];
+			delete[] numbers.v;
+			numbers.v = temp;
+		}
+		numbers.v[numbers.length++] = x;
+	}
+
+	for (size_t i = 0; i < numbers.length; ++i) 
+		cout << numbers.v[i] << ",";
+
+	cout << endl;
+
+	return 0;
+}
