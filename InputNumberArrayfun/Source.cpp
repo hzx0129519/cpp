f9a7811ccb80381fedef154396681189b99f3003
hzx0129519 <zixuan@u.nus.edu>
2019-09-25 18:10:24 +0800

    updated

--- InputNumberArrayfun/Source.cpp
+++ InputNumberArrayfun/Source.cpp
@@ -13,31 +13,48 @@ struct myArray {
 	int *v; // vector
 };
 
+int getInteger(const char* promptMsg) {
+	int x;
+	do {
+		cout << promptMsg << endl;
+		cin >> x;
+	} while (cin.bad());
+	return x;
+}
+
+void printArray(const myArray& n) {
+	for (size_t i = 0; i < n.length; ++i)
+		cout << n.v[i] << ",";
+	cout << endl;
+}
+
+void growVector(myArray& n){
+	int* temp = new int[n.capacity *= n.growth_factor];
+	for (size_t i = 0; i < n.length; ++i)
+		temp[i] = n.v[i];
+	delete[] n.v;
+	n.v = temp;
+}
+
+void pushBack(myArray& n, int x) {
+	if (n.length == n.capacity)
+		growVector(n);
+	n.v[n.length++] = x;
+}
+
+
 int main() {
-	int x; 
 	myArray numbers;
 	numbers.v = new int[numbers.capacity];
 	
 	while (true) {
-		cout << "Please input a numebr (program terminated once a -ve number is entered)" << endl;
-		cin >> x;
-		if (x < 0) 
-			break;
+		int x = getInteger("Please input a numebr (program terminated once a -ve number is entered)");
+		if (x < 0) break;
 
-		if (numbers.length == numbers.capacity) {
-			int *temp = new int[numbers.capacity *= numbers.growth_factor];
-			for (size_t i = 0; i < numbers.length; ++i)
-				temp[i] = numbers.v[i];
-			delete[] numbers.v;
-			numbers.v = temp;
-		}
-		numbers.v[numbers.length++] = x;
+		pushBack(numbers, x);
 	}
 
-	for (size_t i = 0; i < numbers.length; ++i) 
-		cout << numbers.v[i] << ",";
-
-	cout << endl;
+	printArray(numbers);
 
 	return 0;
 }
---
+++ InputNumberArrayfun/using_struct.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<iomanip> // for formatting the output
+#include<cmath> // for calculation 
+
+using namespace std;
+
+struct myArray {
+	const size_t initial_size = 2;
+	const size_t growth_factor = 2;
+	size_t length = 0;
+	size_t capacity = initial_size;
+
+	int* v; // vector
+};
+
+int main() {
+	int x;
+	myArray numbers;
+	numbers.v = new int[numbers.capacity];
+
+	while (true) {
+		cout << "Please input a numebr (program terminated once a -ve number is entered)" << endl;
+		cin >> x;
+		if (x < 0)
+			break;
+
+		if (numbers.length == numbers.capacity) {
+			int* temp = new int[numbers.capacity *= numbers.growth_factor];
+			for (size_t i = 0; i < numbers.length; ++i)
+				temp[i] = numbers.v[i];
+			delete[] numbers.v;
+			numbers.v = temp;
+		}
+		numbers.v[numbers.length++] = x;
+	}
+
+	for (size_t i = 0; i < numbers.length; ++i)
+		cout << numbers.v[i] << ",";
+
+	cout << endl;
+
+	return 0;
+}
